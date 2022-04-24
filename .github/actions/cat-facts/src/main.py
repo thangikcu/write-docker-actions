44a4da62e9c2cb592c1b17e9794572da1f3ce034
Thanggun99 <thanggun99@gmail.com>
2022-04-24 10:38:42 +0700

    Update main.py

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -20,7 +20,7 @@ for fact in r_obj_list:
 # Select a random fact from the fact_list and return it
 # into a variable named random_fact so we can use it
 def select_random_fact(fact_arr):
-    if len(fact_list) == 0
+    if len(fact_list) == 0:
         return "Cat is cat"
     return fact_arr[random.randint(0, len(fact_list)+1)]
 
